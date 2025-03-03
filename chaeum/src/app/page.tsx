b2f533f214ac6d40d5873518a6d8093d30b820ef
Iseo Choi <chtoqur@gmail.com>
2025-03-04 04:46:32 +0900

    [feat] 헤더 구현(#9)

--- chaeum/src/app/page.tsx
+++ chaeum/src/app/page.tsx
@@ -1,7 +1,16 @@
+import Header from '@/components/Header';
+import Image from 'next/image';
+import MainImage from 'public/images/test-main.png';
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">CHAEUM</h1>
+    <div className="flex-col min-h-screen items-center justify-center bg-gray-100">
+      <div className="relative">
+        <Image src={MainImage} alt="테스트 메인 이미지" className="w-full" />
+        <div className="absolute top-0 left-0 right-0 z-10">
+          <Header />
+        </div>
+      </div>
     </div>
   );
 }
--- chaeum/src/styles/colors.ts
+++ chaeum/src/styles/colors.ts
@@ -3,8 +3,11 @@ const colors = {
   lightBlack: '#333333',
   black: '#000000',
   lightGray: '#F6F6F6',
+  lightGray_200: '#F0F0F0',
   mediumGray: '#CAC9C9',
+  darkGray: '#848484',
   deepBlue: '#01597D',
+  darkBrown: '#4D4640',
 };
 
 export default colors;
